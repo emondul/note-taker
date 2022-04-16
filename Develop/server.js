9cd6e7fa2527f2663c3f5b6b26bbc658eb0bee15
Emma Mondul <emondul@gmail.com>
2022-04-16 14:17:55 -0700

    get route

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.get('/api/notes', async (req,res) => {
+    let dbData = [];
+    const readFileAsync = util.promisify(fs.readFile);
+    const freshDB = await readFileAsync('./db/db.json', 'utf8');
+    dbData = [].concat(JSON.parse(freshDB));
+    console.log(dbData);
+    res.json(dbData);
+  })
